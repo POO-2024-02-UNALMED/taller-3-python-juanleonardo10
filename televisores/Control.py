4a88b50d20364773cd5e06dc78fc4a47fdec644a
juanleonardo10 <159485131+juanleonardo10@users.noreply.github.com>
2024-11-19 16:09:17 +0000

    Ya no se que mas hacer

--- televisores/Control.py
+++ televisores/Control.py
@@ -1,42 +1,26 @@
 class Control:
-    def _init_(self):
-        self._tv = None
-
-    def enlazar(self, tv):
-        self._tv = tv
-        if tv is not None:
-            tv.setControl(self)
-
+    def __init__(self):
+        self._tv=None
+    def enlazar(self,  tv):
+        self._tv=tv
+        tv.setControl(self)
     def turnOn(self):
-        if self._tv:
-            self._tv.turnOn()
-
+        self._tv.turnOn()
     def turnOff(self):
-        if self._tv:
-            self._tv.turnOff()
-
+        self._tv.turnOff()
     def canalUp(self):
-        if self._tv:
-            self._tv.canalUp()
-
+        self._tv.canalUp()
     def canalDown(self):
-        if self._tv:
-            self._tv.canalDown()
-
+        self._tv.canalDown()
     def volumenUp(self):
-        if self._tv:
-            self._tv.volumenUp()
-
+        self._tv.volumenUp()
     def volumenDown(self):
-        if self._tv:
-            self._tv.volumenDown()
-
-    def setCanal(self, canal):
-        if self._tv:
-            self._tv.setCanal(canal)
-
+        self._tv.volumenDown()
+    def setTv(self, tv):
+        self._tv=tv
     def getTv(self):
         return self._tv
-
-    def setTv(self, tv):
-        self._tv = tv
+    def setCanal(self,canal):
+        self._tv.setCanal(canal)
+    def setVolumen(self, volumen):
+        self._tv.setVolumen(volumen)
